fffa5dc86c50905c81087bf67f702e9d1ae777d0
amandamachadodev <machado.amandag@gmail.com>
2024-02-03 11:15:34 -0300

    feat: req 4 - implementado metodo getClosestMuseum da camada serviço

---
+++ src/main/java/com/betrybe/museumfinder/exception/MuseumNotFoundException.java
@@ -0,0 +1,9 @@
+package com.betrybe.museumfinder.exception;
+
+/**
+ * Unchecked Excpetion Not found.
+ */
+public class MuseumNotFoundException extends RuntimeException {
+  public MuseumNotFoundException(String message) {
+  }
+}
